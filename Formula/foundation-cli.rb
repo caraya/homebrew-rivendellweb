09e18577c700b8fde3d7c3458888357c24111bff
Carlos Araya <carlos.araya@gmail.com>
2021-09-27 05:32:31 +0000

    foundation-cli: add 2.2.7 bottle.

--- Formula/foundation-cli.rb
+++ Formula/foundation-cli.rb
@@ -7,6 +7,12 @@ class FoundationCli < Formula
   sha256 "f2004ae3c68dc52ad20832ba46baac43f39ada1a03931238f5c1d3d72548d99e"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/caraya/homebrew-rivendellweb/releases/download/foundation-cli-2.2.7"
+    sha256 cellar: :any_skip_relocation, catalina:     "3d204f3fbd5c3328717b1d27db0ddc0752dd51cb2f57cab57faeca21b916b486"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1c07f140486b7fd42040193b7d20ffd01154c1a082464e1d420be33d8f2320a6"
+  end
+
   depends_on "node"
 
   def install
