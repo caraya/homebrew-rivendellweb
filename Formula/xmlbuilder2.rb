9a910d33072540a2e412d0a9a6f345495c88f1d1
Carlos <carlos.araya@gmail.com>
2021-09-26 20:14:19 -0700

    xmlbuilder2 (new formula)

---
+++ Formula/xmlbuilder2.rb
@@ -0,0 +1,24 @@
+require "language/node"
+
+class Xmlbuilder2 < Formula
+  desc "XML builder for node.js"
+  homepage "https://github.com/oozcitak/xmlbuilder2"
+  url "https://registry.npmjs.org/xmlbuilder2/-/xmlbuilder2-3.0.2.tgz"
+  sha256 "b3a9bb4194aff1943d3be8a8353176123c5af5d65c0e589ff04f2275772c4848"
+  license "MIT"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    raise "Test not implemented."
+  end
+end
