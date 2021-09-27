a199fe720eed4e07c93e1efe1fa6005bc852ecdf
Carlos Araya <carlos.araya@gmail.com>
2021-09-27 03:53:30 +0000

    xmlbuilder2: add 3.0.2 bottle.

--- Formula/xmlbuilder2.rb
+++ Formula/xmlbuilder2.rb
@@ -11,6 +11,12 @@ class Xmlbuilder2 < Formula
     url :stable
   end
 
+  bottle do
+    root_url "https://github.com/caraya/homebrew-rivendellweb/releases/download/xmlbuilder2-3.0.2"
+    sha256 cellar: :any_skip_relocation, catalina:     "aea5c77c968075e47f443a339ffebb7661f6039b4be2305a322da667798fe5c3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "cf5b0d0e6bd327b5bc729b18711f8d6c8a7f8de32aa06d1aa3b90045d427780f"
+  end
+
   depends_on "node"
 
   def install
