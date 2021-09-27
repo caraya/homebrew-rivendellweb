88281098dc8757fb7cd76fdcaa71f0360b0e34af
Carlos <carlos.araya@gmail.com>
2021-09-26 22:09:47 -0700

    fixed audit errors

--- Formula/foundation-cli.rb
+++ Formula/foundation-cli.rb
@@ -2,7 +2,7 @@ require "language/node"
 
 class FoundationCli < Formula
   desc "Command-line installer for Foundation family of frameworks"
-  homepage ""
+  homepage "https://github.com/caraya/foundation-cli/"
   url "https://github.com/caraya/foundation-cli/archive/refs/tags/v2.2.7.tar.gz"
   sha256 "f2004ae3c68dc52ad20832ba46baac43f39ada1a03931238f5c1d3d72548d99e"
   license "MIT"
@@ -10,7 +10,6 @@ class FoundationCli < Formula
   depends_on "node"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
