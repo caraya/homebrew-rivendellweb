6f5057dc637fbc0f30c4a6b842d5d3efbeca9eac
Carlos <carlos.araya@gmail.com>
2021-09-26 22:00:04 -0700

    Redid the formula trying to fix errors

--- Formula/foundation-cli.rb
+++ Formula/foundation-cli.rb
@@ -1,18 +1,21 @@
+require "language/node"
+
 class FoundationCli < Formula
   desc "Command-line installer for Foundation family of frameworks"
-  homepage "https://github.com/caraya/foundation-cli/"
+  homepage ""
   url "https://github.com/caraya/foundation-cli/archive/refs/tags/v2.2.7.tar.gz"
-  sha256 "cded4a08e3fd8f7e3329784076c6067fb34a2a598b96c6424a34e677cbd9f831"
+  sha256 "f2004ae3c68dc52ad20832ba46baac43f39ada1a03931238f5c1d3d72548d99e"
   license "MIT"
 
   depends_on "node"
 
   def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
-    puts "test under development"
+    puts "Tests under development"
   end
 end
