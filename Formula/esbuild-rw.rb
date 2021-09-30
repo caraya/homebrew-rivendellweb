3733fbce0ec0344a7f667cdddce4fa3327d18e9c
Carlos Araya <carlos.araya@gmail.com>
2021-09-30 02:09:09 +0000

    esbuild-rw: add 0.13.3 bottle.

--- Formula/esbuild-rw.rb
+++ Formula/esbuild-rw.rb
@@ -7,6 +7,12 @@ class EsbuildRw < Formula
   sha256 "da5ed75590a694b28901a3af8a761f6a1e90feac77cf2d758dc36d8a89db3a02"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/caraya/homebrew-rivendellweb/releases/download/esbuild-rw-0.13.3"
+    sha256 cellar: :any_skip_relocation, catalina:     "b446dcfcc37033482b090eec88d7f95eece88702135779204277d260144976d8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5327147419f7613e8a1d1c51a859498dc04cb6dcfc17a4e9ed55e01631b84e2b"
+  end
+
   depends_on "node"
 
   def install
